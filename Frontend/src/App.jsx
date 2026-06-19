e270e4a18f665a18e1480044235ed609de6f9ce2
Abhay <abhaypandey0037@gmail.com>
2026-06-19 15:37:53 +0530

    Added some basic components for the UI of different pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import { BrowserRouter } from "react-router-dom"
+import "./App.css"
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/components/InputBox.jsx
@@ -0,0 +1,10 @@
+export function InputBox({label,placeholder}){
+    return(
+        <div>
+            <div className = "text-sm font-medium text-left py-2 px-1">
+                {label}
+            </div>
+            <input placeholder = {placeholder} className = "border border-gray-300 rounded-md px-4 py-1"></input>
+        </div>
+    )
+}
